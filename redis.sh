f096d159bf26e6a102346e483945752ef3aaa90f
cellargalaxy <cellargalaxy@gmail.com>
2018-10-03 16:32:15 +0800

    first commit

---
+++ redis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo 'remember modify configuration'
+echo 'maxmemory'
+echo 'requirepass'
+echo 'press any key to continue or ctrl+c to stop'
+read
+chmod -R u=rwx,g=r--,o=r-- ./*
+docker-compose -f redis.yml up -d
